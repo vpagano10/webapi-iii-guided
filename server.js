5f7095cb9be62d1b6b72131e29ea7e483b7c88e4
vpagano10 <vpagano10@gmail.com>
2019-12-04 13:12:09 -0500

    attempting gatekeeper custom middleware

--- server.js
+++ server.js
@@ -1,20 +1,48 @@
 const express = require('express'); // importing a CommonJS module
+const helmet = require('helmet');
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
-server.use(express.json());
+// custom middleware
+function logger(req, res, next) {
+  console.log(`${req.method} to ${req.originalUrl}`);
+  next();
+};
 
-server.use('/api/hubs', hubsRouter);
+function gatekeeper(req, res, next) {
+  if (req.password === 'mellon') {
+    next()
+  } else {
+    res.status(401)
+      .json({ message: 'You shall not pass!' })
+  }
+}
+
+// middleware
+server.use(helmet()); // > this is how to apply middleware globally
+server.use(express.json());
+server.use(logger);
+
+
+// endpoints
+server.use('/api/hubs', helmet(), hubsRouter);
 
 server.get('/', (req, res) => {
   const nameInsert = (req.name) ? ` ${req.name}` : '';
-
   res.send(`
     <h2>Lambda Hubs API</h2>
     <p>Welcome${nameInsert} to the Lambda Hubs API</p>
     `);
+});
+
+server.get('/echo', (req, res) => {
+  res.send(req.headers)
+});
+
+server.get('/area51', helmet(), gatekeeper(), (req, res) => {
+  res.send(req.headers)
 });
 
 module.exports = server;
